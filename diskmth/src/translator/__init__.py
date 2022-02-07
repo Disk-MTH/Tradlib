cb7f11c09a043bf7a508a7d8924b6592630d47dc
Disk-MTH <gillet.mat@free.fr>
2022-02-07 16:05:47 +0100

    write docstrings

--- diskmth/src/translator/__init__.py
+++ diskmth/src/translator/__init__.py
@@ -1,3 +1,14 @@
 # -*- encoding: utf-8 -*-
 
 from translator import *
+
+"""
+            Sorry for potential spelling mistakes but I'm French and I'm not so good in English
+
+Author: Disk_MTH
+GitHub: https://github.com/Disk-MTH/Translator
+License: MIT
+
+For more information about the usage of this library read the full documentation at :
+        https://github.com/Disk-MTH/Translator/blob/master/diskmth/README.md
+"""
--- diskmth/src/translator/translator.py
+++ diskmth/src/translator/translator.py
@@ -6,11 +6,30 @@ import json
 import functools
 import operator
 
-translations_path = ""
+translations_path = os.getcwd()
 translations = []
+
+"""
+            Sorry for potential spelling mistakes but I'm French and I'm not so good in English
+Author: Disk_MTH
+GitHub: https://github.com/Disk-MTH/Translator
+License: MIT
+
+For more information about the usage of this library read the full documentation at :
+        https://github.com/Disk-MTH/Translator/blob/master/diskmth/README.md
+"""
 
 
 def set_translations_files_path(full_path, flat_build=False, build_architecture=""):
+    """
+    This function is used to define the access path to the translation files
+    Args:
+        - full_path (required): The full path to the folder containing the uncompiled translation files
+        - flat_build (optional): Put true if all your translation files are at the root of your project when compiling
+          otherwise leave false
+        - build_architecture (optional): The full path for your translation files from the compiled project root
+    """
+
     global translations_path
 
     try:
@@ -24,10 +43,18 @@ def set_translations_files_path(full_path, flat_build=False, build_architecture=
 
 
 def get_translations_files_path():
+    """
+    This function returns the path of your translation files. If you haven't setup this path with the
+    "set_translations_files_path()" function, this will return the current work directory.
+    """
     return translations_path
 
 
 def load_translations_files():
+    """
+    This function loads your translation files. If you don't have executed "set_translations_files_path()",
+    translations files in the current work directory will be load.
+    """
     global translations
 
     for file in os.listdir(translations_path):
@@ -40,6 +67,10 @@ def load_translations_files():
 
 
 def get_available_languages():
+    """
+    This function returns the list of available languages. If you don't have executed "load_translations_files",
+    this will return an empty list.
+    """
     available_languages = []
 
     for translation in translations:
@@ -52,11 +83,17 @@ def get_available_languages():
 
 
 def get_translation(language, keys_list):
+    """
+    This function returns the translation associated with the list of keys given with arguments.
+    Args:
+        - language (required): The language (among the list of available languages) in which you want a translation
+        - keys_list (required): The list of keys (in order) allowing access to the desired translation
+    """
     available_languages = get_available_languages()
     language_index = 0
+
     for translation in available_languages:
         if translation == language:
             language_index = available_languages.index(translation)
             break
     return functools.reduce(operator.getitem, keys_list, translations[language_index])
-
